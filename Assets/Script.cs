3aa7ff9458f271c4045147a2221ebead0aaa4c0e
Abdusamad Nigmatov <nigmatovdev@gmail.com>
2026-04-11 15:08:26 +0500

    Script updated

--- Assets/Script.cs
+++ Assets/Script.cs
@@ -5,7 +5,11 @@ public class Script : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        // simmple counter
+        for (int i = 0; i < 10; i++)
+        {
+            Debug.Log(i);
+        }
     }
 
     // Update is called once per frame
